a12c6065ecac4938998d495c48b29e4a0079f821
Dylan <dylanmcleer96@gmail.com>
2019-04-21 20:03:22 +0100

    News added to app routing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 import { LandingComponent } from './landing/landing.component';
+import { NewsComponent } from './news/news.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'weather', component: WeatherComponent }
+  { path: 'weather', component: WeatherComponent },
+  { path: 'news', component: NewsComponent }
 ];
