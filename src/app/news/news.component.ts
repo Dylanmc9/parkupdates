b8ab8e25f1e90148a5c35d7ccd604c2ce900cd49
Dylan <dylanmcleer96@gmail.com>
2019-04-22 12:21:21 +0100

    Functionality added to news.ts file to pull API feed

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WordpressService } from '../wordpress.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  posts$: Observable<any[]>;
+  
+  constructor(private wp: WordpressService) {
+    this.posts$ = this.wp.getPosts();
   }
 
 }
