ad84dd8c4935e4c2dc74988b7a695321caae54ca
Dylan <dylanmcleer96@gmail.com>
2019-04-27 14:46:14 +0100

    Location added to app routing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 import { LandingComponent } from './landing/landing.component';
 import { NewsComponent } from './news/news.component';
-
+import { LocationComponent } from './location/location.component';
 export const AppRoutes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'weather', component: WeatherComponent },
-  { path: 'news', component: NewsComponent }
+  { path: 'news', component: NewsComponent },
+  { path: 'location', component: LocationComponent }
 ];
