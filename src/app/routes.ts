ae9822e125052aebf8b8d5786db30b8cfdf4d77a
Dylan <dylanmcleer96@gmail.com>
2019-05-04 17:17:57 +0100

    Home component HTML/SCSS content created.
    Home page displays cards which link to each component on the application.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,11 +4,14 @@ import { LandingComponent } from './landing/landing.component';
 import { NewsComponent } from './news/news.component';
 import { LocationComponent } from './location/location.component';
 import { ParkmapComponent } from './parkmap/parkmap.component';
+import { HomeComponent } from './home/home.component';
+
 
 export const AppRoutes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'weather', component: WeatherComponent },
   { path: 'news', component: NewsComponent },
   { path: 'location', component: LocationComponent },
-  { path: 'parks', component: ParkmapComponent  }
+  { path: 'parks', component: ParkmapComponent  },
+  { path: 'home', component: HomeComponent  }
 ];
